c0d57482f2a94e8aed0fedc624861700c790b6d5
jks7878 <67408890+jks7878@users.noreply.github.com>
2021-04-12 18:34:00 +0900

    2021-04-12 설정 미완

--- src/main/java/com/pro/threeb/HomeController.java
+++ src/main/java/com/pro/threeb/HomeController.java
@@ -1,15 +1,14 @@
 package com.pro.threeb;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -23,17 +22,13 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	private String main(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		/*
+		 * String viewName = (String)request.getAttribute("viewName");
+		 * System.out.println("viewName : " + viewName); ModelAndView mav = new
+		 * ModelAndView(viewName);
+		 */
+		return "main";
 	}
 	
 }
